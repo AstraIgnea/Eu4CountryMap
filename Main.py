4d60eda556ed77a4588d76aa5c6ee8585393cf75
hb321 <hb321@ic.ac.uk>
2022-04-23 01:07:20 +0100

    Implemented Negation

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ if New.lower() == "y":
 else:
     original = np.array(Image.open("Blank.png"))
 mapcolours = original.reshape(int(original.size/3),1,3)
-def update(nation):
+def update(nation,negate):
     countries = open("Combination","r")
     nations = [line.split(",") for line in countries.read().split("\n")]
     info = []
@@ -19,16 +19,26 @@ def update(nation):
     if info == []:
         print("Not a recognised nation")
         return()
-    colour = np.array([int(info[1]),int(info[2]),int(info[3])])
+    if negate == True:
+        colour = np.array([0,0,0])
+    else:
+        colour = np.array([int(info[1]),int(info[2]),int(info[3])])
     print(colour)
     for pixel in info[4:]:
         mapcolours[int(pixel)] = colour
 
 while True:
     a = input("INPUT COUNTRY OR ENTER DONE TO FINISH\n>>>")
-    if a == "DONE":
+    if a[0] == "-":
+        if a[1] == "n":
+            update(a[3:],True)
+        else:
+            print("Not a recognised command")
+    elif a == "DONE":
         break
-    update(a)
+    else:
+        update(a,False)
+
 blank = mapcolours.reshape(original.shape)
 newImage = Image.fromarray(blank)
 newImage.show()
